fa203ad56782cf8c4ec9470107004bc1cfdfa355
Pawel Maliszewski <pablo999455@gmail.com>
2023-06-18 16:48:36 +0200

    solution

--- src/main/java/pl/javastart/euler2/Euler2.java
+++ src/main/java/pl/javastart/euler2/Euler2.java
@@ -1,8 +1,46 @@
 package pl.javastart.euler2;
 
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
 public class Euler2 {
 
-    public static void main(String[] args) {
-
+    public long returnFibSumForEvenNumbers(long maxFibValue) {
+        boolean stop = false;
+        long sum = 0;
+        long count = 1;
+        do {
+            long number = fibNumberByQueue(count);
+            if (number % 2 == 0) {
+                if (number <= maxFibValue) {
+                    sum += number;
+                } else {
+                    stop = true;
+                }
+            }
+            count++;
+        } while (!stop);
+        return sum;
     }
+
+    private long fibNumberByQueue(long fibNumber) { // co sądzicie o takiej metodzie do obliczenia fib ????
+        Queue<Long> fibQueue = new LinkedList<>();
+        fibQueue.add(0L);
+        fibQueue.add(1L);
+        for (long i = 0; i < fibNumber; i++) {
+            fibQueue.add(Objects.requireNonNull(fibQueue.poll()) + Objects.requireNonNull(fibQueue.peek()));
+        }
+        return Objects.requireNonNull(fibQueue.peek());
+    }
+
+//    public long fibRecursion(long number) {
+//        if (number == 0) {
+//            return 0;
+//        } else if (number == 1) {
+//            return 1;
+//        } else {
+//            return fibRecursion(number - 1) + fibRecursion(number - 2);
+//        }
+//    }
 }
---
+++ src/test/java/pl/javastart/euler2/Euler2Test.java
@@ -0,0 +1,36 @@
+package pl.javastart.euler2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Euler2Test {
+    private Euler2 euler2;
+
+    @Test
+    public void should_Return_Sum_Of_All_Even_Fib_Numbers_Less_Than_4_000_000() {
+
+        euler2 = new Euler2();
+
+        assertEquals(4613732, euler2.returnFibSumForEvenNumbers(4000000));
+    }
+
+    @Test
+    public void should_Return_Sum_Of_All_Even_Fib_Numbers_Less_Than_20_000_000() {
+
+        euler2 = new Euler2();
+
+        assertEquals(19544084, euler2.returnFibSumForEvenNumbers(20000000));
+    }
+    @ParameterizedTest(name = "{arguments} expected number for Maximum Fibonacci Value")
+    @CsvSource({"0, 0","2, 3","10, 10","44, 34","10, 30","44, 40","44, 50"
+            ,"44, 80","188, 200","188, 400","798, 1000", })
+    public void should_Return_Correct_Number_For_SUM_Of_All_Fib_Number_With_Max_Value(long expected, long fibNumber) {
+        euler2 = new Euler2();
+
+        assertEquals(expected, euler2.returnFibSumForEvenNumbers(fibNumber));
+    }
+
+}
